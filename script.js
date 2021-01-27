4fd73202f6ce48296eac618b9a2d6d76ebe41b64
SwiftSki <49699645+SwiftSki@users.noreply.github.com>
2021-01-26 22:32:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+var ip;
+fetch('https://ifconfig.me/ip').then(res => res.text()).then(data => ip = data);
 function send(){
-  alert('sent shit');
+  alert(ip);
+  let req = new XMLHttpRequest();
+  req.open("POST", "https://discord.com/api/webhooks/803856738791981108/X4ZKThI27w7EagPQ6n1GvOybOxy0EWv2H8kVNrwj-c38wIEI90tEqoXAk5-9kbwx3prR");
+  req.setRequestHeader('Content-type', 'application/json');
+  let params = {
+    username: ip,
+    avatar_url: "",
+    content: (document.getElementById('input').value)
+  }
+  req.send(JSON.stringify(params));
 }
